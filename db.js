b7e8894ffe601bd1caca122fedb839b21c5c61b6
felix <ifyifeanyi301@gmail.com>
2026-03-18 16:24:41 +0100

    `Added DATABASE_URL and SSL configuration to db.js`

--- db.js
+++ db.js
@@ -6,10 +6,12 @@ dotenv.config();
 const { Pool } = pg;
 
 const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'psotgres',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'skywave_db',
+  ssl: { rejectUnauthorized: false },
   waitForConnections: true,
   queueLimit: 0
 });
